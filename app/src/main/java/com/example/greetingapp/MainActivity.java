6643b915497a486a9c822250d342c9e02684df6c
hiroyuki0402 <hiro1990.cloud@gmail.comcd>
2023-10-08 17:27:31 +0900

    トースト

--- app/src/main/java/com/example/greetingapp/MainActivity.java
+++ app/src/main/java/com/example/greetingapp/MainActivity.java
@@ -3,12 +3,35 @@ package com.example.greetingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button doneButton;
+    EditText inputed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        doneButton = findViewById(R.id.doneButton);
+        inputed = findViewById(R.id.editTextText);
+
+        doneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String inputName =  inputed.getText().toString();
+                String showMessage = "ようこそ" + inputName + "テストアプリへ";
+
+                Toast.makeText(MainActivity.this, showMessage, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+
 }
